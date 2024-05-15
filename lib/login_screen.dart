0daf9d6bf194cb67c7c57d2872d48c8d37e2e880
Farhad Dubey <farhaddubey003@gmail.com>
2024-05-15 09:23:30 +0530

    bloc_cosmuner

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -18,67 +18,72 @@ class _LoginScreenState extends State<LoginScreen>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: BlocListener<AuthBloc, AuthState>(
-        listener: (context, state){
-          if(state is AuthFailure){
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.error)));
-          }
-          if(state is AuthSuccess){
-            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>const HomeScreen(),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state)
+    {
+      if (state is AuthFailure) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.error)));
+      }
+      if (state is AuthSuccess) {
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen(),
             ), (route) => false);
-          }
-        //   if(state is AuthLoading){
-        //     return CircularProgressIndicator();
-        //   // } // Content inside listerner cannor work as bloc builder hence blocbuilder required.
-        },
-        child: BlocBuilder<AuthBloc, AuthState>(
+      }
+    },
           builder: (context, state) {
-            if(state is AuthLoading){
+            if (state is AuthLoading) {
               return const Center(child: CircularProgressIndicator());
             }
             return SingleChildScrollView(
-                child: Center(
-                  child: Column(
-                    children: [
-                      Image.asset('assets/images/signin_balls.png'),
-                      const Text(
-                        'Sign in',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize:50),
-                      ),
-                      const SizedBox(height: 50,),
-                      const SocialButton(
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/signin_balls.png'),
+                    const Text(
+                      'Sign in',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 50),
+                    ),
+                    const SizedBox(height: 50,),
+                    const SocialButton(
                         iconPath: 'assets/svgs/g_logo.svg',
                         label: 'Continue with Google'
-                      ),
-                      const SizedBox(height: 20,),
-                      const SocialButton(
-                        iconPath: 'assets/svgs/f_logo.svg',
-                        label: 'Continue with Facebook',
-                        horizontalPadding: 90,
-                      ),
-                      const SizedBox(height: 20,),
-                      const Text('or', style: TextStyle(fontSize: 17,),),
-                      const SizedBox(height: 15,),
-                      LoginField(hintText: 'Email', controller: emailController),
-                      const SizedBox(height: 15,),
-                      LoginField(hintText: 'Password', controller: passwordController),
-                      const SizedBox(height: 20,),
-                      GradientButton(
-                        onPressed: (){
-                          context.read<AuthBloc>().add(AuthLoginRequested(
+                    ),
+                    const SizedBox(height: 20,),
+                    const SocialButton(
+                      iconPath: 'assets/svgs/f_logo.svg',
+                      label: 'Continue with Facebook',
+                      horizontalPadding: 90,
+                    ),
+                    const SizedBox(height: 20,),
+                    const Text('or', style: TextStyle(fontSize: 17,),),
+                    const SizedBox(height: 15,),
+                    LoginField(hintText: 'Email', controller: emailController),
+                    const SizedBox(height: 15,),
+                    LoginField(
+                        hintText: 'Password', controller: passwordController),
+                    const SizedBox(height: 20,),
+                    GradientButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(AuthLoginRequested(
                           email: emailController.text.trim(),
                           password: passwordController.text.trim(),
-                          ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                        ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
-              );
-          }
-        ),
-      ),
+              ),
+            );
+            //   if(state is AuthLoading){
+            //     return CircularProgressIndicator();
+            //   // } // Content inside listerner cannor work as bloc builder hence blocbuilder required.
+          },
+    ),
+
     );
   }
 }
+// 1H38M to begin
